e2b59f2add19162d9476eb25a684f755ccfad32e
Sxliduz <guilhermeplays03@gmail.com>
2021-04-12 14:22:42 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,4 +27,12 @@ public class Board {
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
+	
+	public Piece piece(int row, int column) { // Sobrecarga abaixo
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) { // Sobrecarga
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
